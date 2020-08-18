0e15ca96a83276ec6e7d6ce6ebc24d1735380ec6
e.anisimov <eanisimov@serverflot.ru>
2020-08-18 08:41:45 +0500

    First commit

--- src/test/java/ru/serverflot/mapi/MAPIApplicationTests.java
+++ src/test/java/ru/serverflot/mapi/MAPIApplicationTests.java
@@ -14,28 +14,4 @@ import javax.persistence.Persistence;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MAPIApplicationTests {
-
-	@Test
-	public void insertPaper(Paper paper) {
-		PaperRepository paperRepository;
-
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Paper");
-		EntityManager em = emf.createEntityManager();
-
-		em.getTransaction().begin();
-
-		Paper s1 = new Paper();
-		s1.setClose(5.4);
-
-		em.persist(s1);
-		em.getTransaction().commit();
-
-		emf.close();
-		em.close();
-	}
-
-	@Test
-	public void contextLoads() {
-	}
-
 }
